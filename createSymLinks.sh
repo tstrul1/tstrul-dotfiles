13ef460294d7fc1ea8a8fdd20781c8c4b5c67931
Tzachi Strul <tzachi.strul@kenshoo.com>
2020-08-09 02:10:05 +0300

    Update createSymLinks.sh

--- createSymLinks.sh
+++ createSymLinks.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 echo "Creating symlinks for dotfiles"
+mkdir -p  ~/Github_Projects ~/Credentials
+touch ~/Credentialsaliases.zsh
 ln -s ${PWD}/dotfiles/.* ~
 ln -s ${PWD}/dotfiles/* ~
+
