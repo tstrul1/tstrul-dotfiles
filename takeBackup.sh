5c102a7013a2a22a48bc21c950536029d37221dd
Tzachi Strul <tzachi.strul@kenshoo.com>
2020-08-08 23:20:01 +0300

    versioned backups

--- takeBackup.sh
+++ takeBackup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Creating previous files backup"s
-mkdir .dotfiles-backup
+mkdir -p .dotfiles-backup/`date +%F`
 for dotfile in $(ls -la1 dotfiles/);do
-    mv ~/${dotfile} .dotfiles-backup/${dotfile}
+    mv ~/${dotfile} .dotfiles-backup/`date +%F`/${dotfile}
 done
-mv ~/.oh-my-zsh .dotfiles-backup/
-echo "Finished backing up dotfiles to .dotfiles-backup"
+mv ~/.oh-my-zsh .dotfiles-backup/`date +%F`/
+echo "Finished backing up dotfiles to .dotfiles-backup/`date +%F`"
