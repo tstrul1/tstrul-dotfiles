64da8add2f1b7d2705530c1c7fab72905aac4e21
Tzachi Strul <tstrul@gmeil.com>
2020-08-11 02:15:25 +0300

    added kubeshell alias

--- dotfiles/.aliases.zsh
+++ dotfiles/.aliases.zsh
@@ -5,4 +5,5 @@ fixhost() {
 }
 # Open the github page of the repo you're in, in the browser
 function opengit () { git remote -v | awk 'NR==1{print $2}' | sed -e 's?\.git$??' -e "s?git@?https://?" -e "s?https///?https://?g" | xargs open }
+function kubeshell() { kubectl exec -ti $@ -- /bin/sh; }
 source ~/Credentials/aliases.zsh
